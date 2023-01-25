d03cd0e54f2c2a7ccf04bf6f633ff8f86410e77c
RaoHaseeb1604 <122661717+RaoHaseeb1604@users.noreply.github.com>
2023-01-25 20:53:35 +0500

    upload assi. 1

---
+++ app.js
@@ -0,0 +1,15 @@
+// ***chapter1***
+// Question 1
+alert("welcome to website")
+// Question 2
+alert("Error!please enter a password")
+// Question 3
+alert("welcome to js... \n Happy coding!")
+// Question 4
+alert("welcome to js land...")
+// Question 5
+alert("Happy coding! \n prevent this page from creating additional dialogs")
+// Question 6
+alert("Hello...i can run js through my web browsers console")
+// Question 7
+alert("alert")
